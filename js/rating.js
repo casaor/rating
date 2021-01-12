436e1544a84f4cb4f88c4b4a7381432a93f5111a
carlota sas <casaor@outlook.com>
2021-01-12 14:56:21 +0100

    2a versió

--- js/rating.js
+++ js/rating.js
@@ -5,29 +5,10 @@ function changeColor (star) {
 }
 */
 
-document.addEventListener('click', function(event) { 
-		
-    if (event.target.matches('.star')) {
-        event.preventDefault();
-        var target = event.target;				
-        
-        // find selected star
-        var selected = target.getAttribute( 'data-star' );
-        
-        var stars = document.querySelectorAll('.star');
-        
-        for(var i = 0; i < stars.length; i++) { // loop thru all elements
-          if(i < selected){
-              stars[i].classList.add('selected');
-              stars[i].querySelector('.screen-reader').textContent += " Selected";
-              console.log(stars[i].querySelector('.screen-reader').textContent);
-          } else {
-              stars[i].classList.remove('selected');
-              stars[i].querySelector('.screen-reader').textContent = (i+1) + " Star";
-              if(i > 0){ stars[i].querySelector('.screen-reader').textContent += "s"; } 
-              console.log(stars[i].querySelector('.screen-reader').textContent);
-          }
-        }  
-    }
-
-}, false);
+function vote(rating) {
+    $('#contenedor i')
+        .slice(0, rating)
+        .each(function(){
+            $(this).toggleClass('far fas')
+        })
+}
